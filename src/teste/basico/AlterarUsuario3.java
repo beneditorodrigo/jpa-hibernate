35498e1c102549f315a2f6bfe510a27700090adb
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-06-18 22:54:43 -0300

    Alterando usuario em Estado Não Gerenciado (detach())

--- src/teste/basico/AlterarUsuario3.java
+++ src/teste/basico/AlterarUsuario3.java
@@ -1,5 +1,33 @@
 package teste.basico;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
 public class AlterarUsuario3 {
 
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Usuario usuario = em.find(Usuario.class, 7L);
+		
+		if(usuario instanceof Usuario) {
+			em.detach(usuario);
+			usuario.setNome("Benedito");
+			em.merge(usuario);
+		} else {
+			System.out.println("Usuário não encontrado!");
+		}
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
 }
