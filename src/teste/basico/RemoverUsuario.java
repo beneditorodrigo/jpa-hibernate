07743bcb4b7956502b0f5453d9d463b3dc5ccf60
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-06-18 23:38:55 -0300

    Removendo um registro com o remove()

--- src/teste/basico/RemoverUsuario.java
+++ src/teste/basico/RemoverUsuario.java
@@ -4,23 +4,25 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import modelo.basico.Carro;
+import modelo.basico.Usuario;
 
-public class NovoCarro {
+public class RemoverUsuario {
 
 	public static void main(String[] args) {
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Carro carro = new Carro("OBS22A8", 2015, "Renault", "CLIO", "CINZA");
+		Usuario usuario = em.find(Usuario.class, 8L);
 		
-		em.getTransaction().begin();
-		em.persist(carro);
-		em.getTransaction().commit();
+		if(usuario instanceof Usuario) {
+			em.getTransaction().begin();
+			em.remove(usuario);
+			em.getTransaction().commit();
+		} else {
+			System.out.println("Usuário não encontrada!");
+		}
 		
 		em.close();
 		emf.close();
 	}
-
 }
