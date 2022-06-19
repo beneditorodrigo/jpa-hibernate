66fcecc3fc5cc821a08904dad3ef4aa4bcbeb6f6
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-06-18 22:37:09 -0300

    Alterando registro SEM usar merge()

---
+++ src/teste/basico/AlterarUsuario2.java
@@ -0,0 +1,30 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class AlterarUsuario2 {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		Usuario usuario = em.find(Usuario.class, 7L);
+		
+		if(usuario instanceof Usuario) {
+			usuario.setNome("Benedito Cunha");
+		} else {
+			System.out.println("Usuário não encontrado!");
+		}
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
+}
